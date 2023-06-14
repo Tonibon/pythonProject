3d6d80dd6d58956e00e7f51b3a55ffb5f5e04c75
Tonibon <136203194+Tonibon@users.noreply.github.com>
2023-06-14 18:29:50 +0100

    Add files via upload

--- rusty project calculator.py
+++ rusty project calculator.py
@@ -1,20 +1,45 @@
 first_num = float(input("insert first value: "))
+second_num = float(input("input second value: "))
 print(" 1 for addition")
 print(" 2 for subtraction")
 print(" 3 for multiplication")
 print(" 4 for division")
-operation = input("select math operation: ")
-second_num = float(input("input second value: "))
-if operation == "1":
-    print ("your answer is: ", (first_num+second_num))
-elif operation == "2":
-    print ("your answer is: ", (first_num-second_num))
-elif operation == "3":
-    print ("your answer is: ", (first_num*second_num))
-elif operation == "4":
-    if second_num == 0.0:
-        print ("cannot divide error")
-    else:
-        print ("your answer is: ", (first_num/second_num))
+while True:
+    operation = input("select math operation: ")
+    if operation == "1":
+        print("your answer is: ", (first_num+second_num))
+    elif operation == "2":
+        print("your answer is: ", (first_num-second_num))
+    elif operation == "3":
+        print("your answer is: ", (first_num*second_num))
+    elif operation == "4":
+        if second_num == 0.0:
+            print("cannot divide error")
+        else:
+            print("your answer is: ", (first_num/second_num))
+    next_calculation = input("Let's do next calculation? (yes/no): ")
+    if next_calculation == "yes":
+        first_num = float(input("insert first value: "))
+        second_num = float(input("input second value: "))
+        print(" 1 for addition")
+        print(" 2 for subtraction")
+        print(" 3 for multiplication")
+        print(" 4 for division")
+        while True:
+            operation = input("select math operation: ")
+            if operation == "1":
+                print("your answer is: ", (first_num + second_num))
+            elif operation == "2":
+                print("your answer is: ", (first_num - second_num))
+            elif operation == "3":
+                print("your answer is: ", (first_num * second_num))
+            elif operation == "4":
+                if second_num == 0.0:
+                    print("cannot divide error")
+                else:
+                    print("your answer is: ", (first_num / second_num))
+    if next_calculation == "no":
+
+        break
 else:
- print("invalid operation")
+    print("invalid operation")

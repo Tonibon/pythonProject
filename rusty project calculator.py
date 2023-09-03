72a3e54feee18f5d6e4eb47803bcfe4f50218abb
Tonibon <136203194+Tonibon@users.noreply.github.com>
2023-09-03 17:24:45 +0100

    Add files via upload

--- rusty project calculator.py
+++ rusty project calculator.py
@@ -17,18 +17,48 @@ def calculate():
             print("cannot divide error")
         else:
             print("your answer is: ", (first_num/second_num))
-    another = input ("Do you want to run another calculation? (yes/no): ")
+    another = input("Do you want to run another calculation? (yes/no): ")
     if another == "yes":
-        calculate()
+        recall()
     elif another == "no":
         print("the end of program")
+
+
 def calculator():
-    next_calculation = input("Let's do a calculation. Shall we?? (yes/no): ")
+    next_calculation = input("Let's do a calculation. Shall we? (yes/no): ")
     if next_calculation == "yes":
         calculate()
     elif next_calculation == "no":
         print("the end of program")
     else:
         print("incorrect value")
+
+
+def recall():
+    new_cal = input("do you want to use previous answer? (yes/no): ")
+    if new_cal == "yes":
+        previous_answer = float(input("insert previous_answer: "))
+        second_num = float(input("input second value: "))
+        print(" 1 for addition")
+        print(" 2 for subtraction")
+        print(" 3 for multiplication")
+        print(" 4 for division")
+        operation = input("select math operation: ")
+        if operation == "1":
+            print("your answer is: ", (previous_answer + second_num))
+        elif operation == "2":
+            print("your answer is: ", (previous_answer - second_num))
+        elif operation == "3":
+            print("your answer is: ", (previous_answer * second_num))
+        elif operation == "4":
+            if second_num == 0.0:
+                print("cannot divide error")
+            else:
+                print("your answer is: ", (previous_answer / second_num))
+        print(recall())
+    elif new_cal == "no":
+        calculate()
+
+
 cal = calculator()
-print (cal)
+print(cal)

b1e84b1aba2449b0a0121a1f05b609dfcda32a7a
Tonibon <136203194+Tonibon@users.noreply.github.com>
2023-10-02 17:10:26 +0100

    Add files via upload

--- rusty project calculator.py
+++ rusty project calculator.py
@@ -1,3 +1,6 @@
+results = 0
+
+
 def calculate():
     first_num = float(input("insert first value: "))
     second_num = float(input("input second value: "))
@@ -6,17 +9,20 @@ def calculate():
     print(" 3 for multiplication")
     print(" 4 for division")
     operation = input("select math operation: ")
-    if operation == "1":
-        print("your answer is: ", (first_num+second_num))
+    if operation == "1" :
+        answer_new = (first_num+second_num)
     elif operation == "2":
-        print("your answer is: ", (first_num-second_num))
+        answer_new = (first_num - second_num)
     elif operation == "3":
-        print("your answer is: ", (first_num*second_num))
+        answer_new = (first_num*second_num)
     elif operation == "4":
         if second_num == 0.0:
             print("cannot divide error")
         else:
-            print("your answer is: ", (first_num/second_num))
+            answer_new = (first_num/second_num)
+    print(answer_new)
+    global results
+    results = answer_new
     another = input("Do you want to run another calculation? (yes/no): ")
     if another == "yes":
         recall()
@@ -37,7 +43,7 @@ def calculator():
 def recall():
     new_cal = input("do you want to use previous answer? (yes/no): ")
     if new_cal == "yes":
-        previous_answer = float(input("insert previous_answer: "))
+        first_num = results
         second_num = float(input("input second value: "))
         print(" 1 for addition")
         print(" 2 for subtraction")
@@ -45,19 +51,26 @@ def recall():
         print(" 4 for division")
         operation = input("select math operation: ")
         if operation == "1":
-            print("your answer is: ", (previous_answer + second_num))
+            answers = (first_num + second_num)
         elif operation == "2":
-            print("your answer is: ", (previous_answer - second_num))
+            answers = (first_num - second_num)
         elif operation == "3":
-            print("your answer is: ", (previous_answer * second_num))
+            answers = (first_num * second_num)
         elif operation == "4":
             if second_num == 0.0:
                 print("cannot divide error")
             else:
-                print("your answer is: ", (previous_answer / second_num))
-        print(recall())
+                answers = (first_num / second_num)
+        print(answers)
+        global answer_new
+        answer_new = answers
+        another = input("Do you want to run another calculation? (yes/no): ")
+        if another == "yes":
+            recall()
+        elif another == "no":
+            print("the end of program")
     elif new_cal == "no":
-        calculate()
+        calculator()
 
 
 cal = calculator()
